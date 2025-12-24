8888510d2800efb5a880bea5b40b6aa3a628c78f
Siddhartha Abhimanyu <skybotsdeveloper@gmail.com>
2025-12-24 21:45:34 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -e
 
-echo "Starting December..."
-cd backend && npm start &
-cd ../frontend && npm run dev &
+echo "Starting December Backend..."
+cd /app/backend
+npm start &
+
+echo "Starting December Frontend..."
+cd /app/frontend
+npm run dev &
+
 wait
